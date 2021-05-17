6ea4aaffb6c7a3eb5b3ac13dc3fcb22429b5ff73
strannic123 <manager350@mail.ru>
2021-05-17 11:56:21 +0300

    add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <div>
+      <p>Какой то почтовый ящик</p>
+      <p class="email">Какая то почта</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style >
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
+
+</style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,10 +1,16 @@
 <template>
-  <h3>ChatRoom</h3>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar";
+
+
 export default {
-  name: "ChatRoom"
+  name: "ChatRoom",
+  components: {Navbar}
 }
 </script>
 
