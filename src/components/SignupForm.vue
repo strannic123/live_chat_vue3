4e8e53ff000c047630ef6bc61396ab99bef94e2d
strannic123 <manager350@mail.ru>
2021-05-17 11:07:26 +0300

    added register request

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -4,28 +4,34 @@
     <input type="text" placeholder="email" required v-model="email">
     <input type="text" placeholder="password" required v-model="password">
     <button>Sign Up</button>
+    <div class="error">{{error}}</div>
   </form>
 </template>
 
 <script>
 import {ref} from "vue"
+import useSignup from "../composables/useSignup";
 
 export default {
   name: "SignupForm",
   setup(){
+    const {error, signup} = useSignup()
+
     const displayName = ref('')
     const email = ref('')
     const password = ref('')
 
-    const handlerSubmit = () => {
-      console.log(displayName, email, password)
+    const handlerSubmit = async () => {
+      await signup(email.value, password.value, displayName.value)
+      console.log("юзер зарегистрирован")
     }
 
     return {
       displayName,
       email,
       password,
-      handlerSubmit
+      handlerSubmit,
+      error
     }
   }
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDm6z8bTWZ6MSsmTSNW7FIIcdZTR5GEG5Y",
@@ -14,6 +15,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export {projectFirestore, timestamp}
+export {projectAuth, projectFirestore, timestamp}
