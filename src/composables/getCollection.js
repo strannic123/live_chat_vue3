48748e07ee801e9062a36ada3822a7bfc19df747
strannic123 <manager350@mail.ru>
2021-05-18 11:53:33 +0300

    added cancellation of subscription for snapshot

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import {ref} from 'vue'
+import {ref, watchEffect} from 'vue'
 import {projectFirestore} from "../firebase/config";
 
 const getCollection = (collection) => {
@@ -8,7 +8,8 @@ const getCollection = (collection) => {
     let collectionRef = projectFirestore.collection(collection)
         .orderBy('createdAt')
 
-    collectionRef.onSnapshot(snap => {
+    const unsub = collectionRef.onSnapshot(snap => {
+        console.log('snapshot get')
         let results = []
         snap.docs.forEach(doc => {
             doc.data().createdAt && results.push({...doc.data(), id: doc.id})
@@ -19,6 +20,9 @@ const getCollection = (collection) => {
             documents.value = null
             error.value = 'could not fetch data'
         })
+    })
+    watchEffect(onInvalidate => {
+        onInvalidate(() => unsub())
     })
     return {error, documents}
 }
